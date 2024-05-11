f2ffd374863674199a0ed014fb576abdc530ff8a
Xavier BRASSOUD <xavier.brassoud@gmail.com>
2024-05-11 14:43:24 +0200

    Bump theme to v1.0.6

--- .config/setup.sh
+++ .config/setup.sh
@@ -17,13 +17,26 @@
 #   * Font Awesome 4.7
 #
 
+# Personal setup:
+# 1) Hugo installed thought snap, permission hugo:ssh-keys
+# 2) ssh-keyscan github.com >> ~/.ssh/known_hosts
+# 3) ~/.gitconfig
+#     [url "git@github.com:XavierBrassoud/hugo-theme-vertica-resume.git"]
+# 	    insteadOf = https://github.com/XavierBrassoud/hugo-theme-vertica-resume
+# 4) ~/.ssh/config
+#     Host github.com
+#       IdentityFile ~/.ssh/<private_key_to_repo>
+# 5) > ssh-add ~/.ssh/<private_key_to_repo>
+# 6) > (export GIT_DATE=2024-01-01T00:00:00; .config/setup.sh)
+
+
 set -e # exit on failure (avoid false success)
 
 
 CONF_DIR=.config
 
 HUGO_ROOT_DIR=.hugo
-HUGO_CONTENT_DIR=$HUGO_ROOT_DIR/content/json_resume
+HUGO_CONTENT_DIR=$HUGO_ROOT_DIR/content
 HUGO_DATA_DIR=$HUGO_ROOT_DIR/data/json_resume
 HUGO_STATIC_DIR=$HUGO_ROOT_DIR/static
 HUGO_ASSETS_DIR=$HUGO_ROOT_DIR/assets
@@ -45,11 +58,11 @@ hugo mod init github.com/XavierBrassoud/autofolio
 cd ..
 [ -e $HUGO_ROOT_DIR/hugo.toml ] && rm $HUGO_ROOT_DIR/hugo.toml
 [ -e $HUGO_ROOT_DIR/config.toml ] && rm $HUGO_ROOT_DIR/config.toml
-mkdir -p $HUGO_CONTENT_DIR
+mkdir -p $HUGO_CONTENT_DIR/json_resume
 mkdir -p $HUGO_DATA_DIR
 
 cp $CONF_DIR/hugo.yaml $HUGO_ROOT_DIR/hugo.yaml
-cp CHANGELOG.md $HUGO_CONTENT_DIR/CHANGELOG.md
+cp CHANGELOG.md $HUGO_CONTENT_DIR/json_resume/CHANGELOG.md
 cp resume_en.json $HUGO_DATA_DIR/en.json
 cp resume_fr.json $HUGO_DATA_DIR/fr.json
 cp $CONF_DIR/favicon.ico $HUGO_STATIC_DIR
@@ -76,8 +89,5 @@ cat $HUGO_DATA_DIR/fr.json | jq '.meta.lastModified = env.GIT_DATE' > $HUGO_DATA
 libreoffice --headless --convert-to pdf resume_en.fodp --outdir $HUGO_ROOT_DIR
 libreoffice --headless --convert-to pdf resume_fr.fodp --outdir $HUGO_ROOT_DIR
 
-mkdir -p $HUGO_STATIC_DIR/en
-mkdir -p $HUGO_STATIC_DIR/fr
-
-mv $HUGO_ROOT_DIR/resume_en.pdf $HUGO_STATIC_DIR/en/cv.pdf
-mv $HUGO_ROOT_DIR/resume_fr.pdf $HUGO_STATIC_DIR/fr/cv.pdf
+mv $HUGO_ROOT_DIR/resume_en.pdf $HUGO_CONTENT_DIR/cv.en.pdf
+mv $HUGO_ROOT_DIR/resume_fr.pdf $HUGO_CONTENT_DIR/cv.fr.pdf
